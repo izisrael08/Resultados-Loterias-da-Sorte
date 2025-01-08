7361bc33cf5aa51ca561fce79d131a13998a0840
israel guedes <israelguedes008@gmail.com>
2025-01-07 21:36:06 -0300

    commit 9

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://resultados-loterias-da-sorte-production.up.railway.app/results'; // URL completa do endpoint
-    
+
 fetch(API_URL)
   .then(response => {
     if (!response.ok) {
@@ -16,44 +16,45 @@ fetch(API_URL)
       return;
     }
 
-      // Ordenando os resultados pela chave "Premio"
-      data.forEach(item => {
-        item.Resultados.sort((a, b) => {
-          const premioA = parseInt(a.Premio.replace('°', '')); // Remove o símbolo "°" e converte para inteiro
-          const premioB = parseInt(b.Premio.replace('°', ''));
-          return premioA - premioB;
-        });
-
-        let content = `
-          <div class="result-card">
-            <div class="result-header">
-              <span class="result-time">${item.Hora}</span>
-              <span class="result-title">${item.Titulo}</span>
-              <span class="result-day">${item.Dia}</span>
-            </div>
-            <div class="result-details">
-              <div class="result-prize"><strong>Prêmio</strong></div>
-              <div class="result-result"><strong>Resultado</strong></div>
-              <div class="result-group"><strong>Grupo</strong></div>
-            </div>
-            <div class="result-values">
-              ${item.Resultados.map((resultado) => {
-                return `
-                  <div class="result-value">
-                    <span class="result-index">${resultado.Premio}</span>           
-                    <span class="result-result-value">${resultado.Resultado}</span>
-                    <span class="result-group-value">${resultado.Grupo}</span>
-                  </div>
-                `;
-              }).join('')}
-            </div>
-          </div>
-        `;
-        resultsDiv.innerHTML += content;
+    // Ordenando os resultados pela chave "Premio"
+    data.forEach(item => {
+      item.Resultados.sort((a, b) => {
+        // Verifica se Premio é uma string válida antes de chamar replace
+        const premioA = a.Premio ? parseInt(a.Premio.replace('°', '')) : 0;
+        const premioB = b.Premio ? parseInt(b.Premio.replace('°', '')) : 0;
+        return premioA - premioB;
       });
-    })
-    .catch(error => {
-      console.error('Erro ao carregar os resultados:', error);
-      const resultsDiv = document.getElementById('results');
-      resultsDiv.innerHTML = `<p>Ocorreu um erro ao carregar os resultados.</p>`;
+
+      let content = `
+        <div class="result-card">
+          <div class="result-header">
+            <span class="result-time">${item.Hora}</span>
+            <span class="result-title">${item.Titulo}</span>
+            <span class="result-day">${item.Dia}</span>
+          </div>
+          <div class="result-details">
+            <div class="result-prize"><strong>Prêmio</strong></div>
+            <div class="result-result"><strong>Resultado</strong></div>
+            <div class="result-group"><strong>Grupo</strong></div>
+          </div>
+          <div class="result-values">
+            ${item.Resultados.map((resultado) => {
+              return `
+                <div class="result-value">
+                  <span class="result-index">${resultado.Premio}</span>           
+                  <span class="result-result-value">${resultado.Resultado}</span>
+                  <span class="result-group-value">${resultado.Grupo}</span>
+                </div>
+              `;
+            }).join('')}
+          </div>
+        </div>
+      `;
+      resultsDiv.innerHTML += content;
     });
+  })
+  .catch(error => {
+    console.error('Erro ao carregar os resultados:', error);
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = `<p>Ocorreu um erro ao carregar os resultados.</p>`;
+  });
